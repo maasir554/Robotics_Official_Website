c1e1547aff1ede0d37a719e7121ee8c8726fb6ed
anupam-dwivedi05 <anupamdwivedi8318@gmail.com>
2025-12-03 19:24:20 +0530

    complete navbar

---
+++ app/Components/HeroSection.jsx
@@ -0,0 +1,19 @@
+const HeroSection = () => {
+  return (
+    <>
+      <div>
+        <div>
+          {/* circle */}
+          <div className="">
+            
+          </div>
+        </div>
+
+        {/* photosection */}
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default HeroSection;
--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -37,39 +37,38 @@ export default function Navbar() {
         </button>
 
         {open && (
-          <ul className="md-hidden text-white absolute top-16 left-0 w-full flex flex-col gap-5 p-6 font-semibold items-center bg-gradient-to-b from-blue-950 to-blue-800">
-  
-  <li className="flex flex-col w-full text-center">
-    <Link href="/" className="block">
-      Home
-    </Link>
-    <hr className="w-full h-[1px] bg-white border-0 mt-2" />
-  </li>
+          <ul className={`md-hidden text-white absolute top-16 left-0 w-full flex flex-col gap-5 font-semibold items-center bg-gradient-to-b from-blue-950 to-blue-800 transition-all duration-700 ease-in-out ${open ? `max-h-[500px]  pt-6 pb-6` : `max-h-0 pt-0 pb-0`}`} >
+            <li className="flex flex-col w-full text-center">   
+              <Link href="/" className="block">
+                Home
+              </Link>
+              <hr className="w-full h-[0.5px] bg-white border-0 mt-2" />
+            </li>
 
-  <li className="flex flex-col w-full text-center">
-    <Link href="/robomax" className="block">
-      RoboMax
-    </Link>
-    <hr className="w-full h-[1px] bg-white border-0 mt-2" />
-  </li>
+            <li className="flex flex-col w-full text-center">
+              <Link href="/robomax" className="block">
+                RoboMax
+              </Link>
+              <hr className="w-full h-[0.5px] bg-white border-0 mt-2" />
+            </li>
 
-  <li className="flex flex-col w-full text-center">
-    <Link href="/ourteam" className="block">
-      Our Team
-    </Link>
-    <hr className="w-full h-[1px] bg-white border-0 mt-2" />
-  </li>
+            <li className="flex flex-col w-full text-center">
+              <Link href="/ourteam" className="block">
+                Our Team
+              </Link>
+              <hr className="w-full h-[0.5px] bg-white border-0 mt-2" />
+            </li>
 
-  <li className="flex flex-col w-full text-center">
-    <Link href="/pastevents" className="block">
-      Past Events
-    </Link>
-    <hr className="w-full h-[1px] bg-white border-0 mt-2" />
-  </li>
-  
-</ul>
+            <li className="flex flex-col w-full text-center">
+              <Link href="/pastevents" className="block">
+                Past Events
+              </Link>
+              <hr className="w-full h-[0.5px] bg-white border-0 mt-2" />
+            </li>
+          </ul>
         )}
       </div>
+      <hr className="w-full h-[0.5px] bg-white border-0 mt-2" />
     </>
   );
 }
